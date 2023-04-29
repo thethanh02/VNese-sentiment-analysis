8c6fa7cda43b44e1c25534988ee3b69f7b3dead5
thethanh02 <2002tintin@gmail.com>
2023-04-29 21:08:58 +0700

    2nd commit

---
+++ app/main.py
@@ -0,0 +1,131 @@
+# Core Pkgs
+import streamlit as st 
+import altair as alt
+from altair import AxisConfig
+import plotly.express as px 
+
+# EDA Pkgs
+import pandas as pd 
+import numpy as np 
+from datetime import datetime
+
+# Utils
+import ktrain
+predictor = None
+def load_predictor():
+	global predictor
+	predictor = ktrain.load_predictor('./saved_model')
+
+# Utils
+from utils import my_normalize_text
+from track_utils import *
+
+from googletrans import Translator
+translator = Translator()
+
+import sqlite3
+def emotiontest_table_exists():
+	with sqlite3.connect("database.db") as conn:
+		c = conn.cursor()
+		listOfTables = c.execute(
+		"""SELECT name FROM sqlite_master WHERE type='table' AND name='emotionTestTable'; """).fetchall()
+		
+		if listOfTables == []:
+			return False
+		return True
+	
+def load_testdata():
+	check = emotiontest_table_exists()
+	if not check:
+		create_emotiontest_table()
+		dataset = pd.read_csv("./test10cau.csv")
+		for i in range(12):
+			raw_text = dataset['Text'][i]
+			probability = get_prediction_proba(raw_text)
+			add_predictiontest_details(raw_text, predictor.predict(raw_text), float(np.max(probability)), dataset['Emotion'][i])
+
+# Fxn
+def predict_emotions(docx):
+	results = predictor.predict([docx])
+	return results[0]
+
+def get_prediction_proba(docx):
+	results = predictor.predict_proba([docx])
+	return results
+
+emotions_emoji_dict = {'thông tin':'😐', 'hạnh phúc':'😊', 'buồn bã':'😔', 'sợ hãi':'😱', 'ngạc nhiên':'😮', 'phẫn nộ':'😠'}
+
+# Main Application
+def main():
+	st.title("Emotion Classifier App")
+	menu = ["Home","Monitor"]
+	choice = st.sidebar.selectbox("Menu",menu)
+	load_predictor()
+	load_testdata()
+	create_emotionclf_table()
+	if choice == "Home":
+		st.subheader("Home-Emotion In Text")
+
+		with st.form(key='emotion_clf_form'):
+			raw_text = st.text_area("Type Here")
+			submit_text = st.form_submit_button(label='Submit')
+
+		if submit_text:
+			col1,col2  = st.columns(2)
+			handled_text = my_normalize_text(raw_text)
+			# Apply Fxn Here
+			prediction = predict_emotions(handled_text)
+			probability = get_prediction_proba(handled_text)
+			# print(prediction)
+			add_prediction_details(raw_text,prediction,float(np.max(probability)),datetime.now())
+			
+			with col1:
+				st.success("Handled Text")
+				st.write(handled_text)
+
+				st.success("Prediction")
+				emoji_icon = emotions_emoji_dict[prediction]
+				st.write("{}:{}".format(prediction,emoji_icon))
+				st.write("Confidence: {}%".format(np.max(probability)*100))
+
+
+			with col2:
+				st.success("Prediction Probability")
+				proba_df = pd.DataFrame(probability,columns=predictor.get_classes())
+				proba_df_clean = proba_df.T.reset_index()
+				proba_df_clean.columns = ["emotions","probability"]
+
+				fig = alt.Chart(proba_df_clean).mark_bar().encode(
+					x='emotions',
+					y='probability',
+					color='emotions')
+							
+				st.altair_chart(fig,use_container_width=True)
+
+
+	elif choice == "Monitor":
+		st.subheader("Monitor App")
+
+		with st.expander("Test Metrics"):
+			df_emotions = pd.DataFrame(view_all_predictiontest_details(),columns=['RawText','Prediction','Probability','RawEmotion'])
+			st.dataframe(df_emotions)
+			df_emotions['index'] = df_emotions.index
+
+			chart = alt.Chart(df_emotions).mark_bar(width=20).encode(
+				x='index',
+				y='Probability',
+				color='Prediction'
+			)
+			st.altair_chart(chart,use_container_width=True)
+
+		with st.expander('Emotion Classifier Metrics'):
+			df_emotions = pd.DataFrame(view_all_prediction_details(),columns=['Rawtext','Prediction','Probability','Time_of_Visit'])
+			st.dataframe(df_emotions)
+
+			prediction_count = df_emotions['Prediction'].value_counts().rename_axis('Prediction').reset_index(name='Counts')
+			pc = alt.Chart(prediction_count).mark_bar().encode(x='Prediction',y='Counts',color='Prediction')
+			st.altair_chart(pc,use_container_width=True)	
+
+
+if __name__ == '__main__':
+	main()
---
+++ app/utils.py
@@ -0,0 +1,75 @@
+import re
+from underthesea import word_tokenize, text_normalize
+import string
+
+def remove_emoji(text):
+    emoji_pattern = re.compile("["
+                                u"\U0001F600-\U0001F64F"  # emoticons
+                                u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                                u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                                u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                                u"\U00002500-\U00002BEF"  # chinese char
+                                u"\U00002702-\U000027B0"
+                                u"\U00002702-\U000027B0"
+                                u"\U000024C2-\U0001F251"
+                                u"\U0001f926-\U0001f937"
+                                u"\U00010000-\U0010ffff"
+                                u"\u2640-\u2642"
+                                u"\u2600-\u2B55"
+                                u"\u200d"
+                                u"\u23cf"
+                                u"\u23e9"
+                                u"\u231a"
+                                u"\ufe0f"  # dingbats
+                                u"\u3030"
+                                "]+", flags=re.UNICODE)
+    return emoji_pattern.sub(r'', text)
+
+def my_normalize_text(text):
+
+    #Remove các ký tự kéo dài: vd: đẹppppppp
+    text = re.sub(r'([A-Z])\1+', lambda m: m.group(1).upper(), text, flags=re.IGNORECASE)
+
+    text = text.lower()
+
+    #Chuẩn hóa tiếng Việt, xử lý emoj, chuẩn hóa tiếng Anh, thuật ngữ
+    replace_list = {
+        'òa': 'oà', 'óa': 'oá', 'ỏa': 'oả', 'õa': 'oã', 'ọa': 'oạ', 'òe': 'oè', 'óe': 'oé','ỏe': 'oẻ',
+        'õe': 'oẽ', 'ọe': 'oẹ', 'ùy': 'uỳ', 'úy': 'uý', 'ủy': 'uỷ', 'ũy': 'uỹ','ụy': 'uỵ', 'uả': 'ủa',
+        'ả': 'ả', 'ố': 'ố', 'u´': 'ố','ỗ': 'ỗ', 'ồ': 'ồ', 'ổ': 'ổ', 'ấ': 'ấ', 'ẫ': 'ẫ', 'ẩ': 'ẩ',
+        'ầ': 'ầ', 'ỏ': 'ỏ', 'ề': 'ề','ễ': 'ễ', 'ắ': 'ắ', 'ủ': 'ủ', 'ế': 'ế', 'ở': 'ở', 'ỉ': 'ỉ',
+        'ẻ': 'ẻ', 'àk': u' à ','aˋ': 'à', 'iˋ': 'ì', 'ă´': 'ắ','ử': 'ử', 'e˜': 'ẽ', 'y˜': 'ỹ', 'a´': 'á',
+        #Chuẩn hóa 1 số sentiment words/English words
+        'ô kêi': ' ok ', 'okie': ' ok ', ' o kê ': ' ok ',
+        'okey': ' ok ', 'ôkê': ' ok ', 'oki': ' ok ', ' oke ':  ' ok ',' okay':' ok ','okê':' ok ',
+        ' tks ': u' cám ơn ', 'thks': u' cám ơn ', 'thanks': u' cám ơn ', 'ths': u' cám ơn ', 'thank': u' cám ơn ',
+        ' khôg ': ' không ', 'kg ': u' không ','not': u' không ', u' kg ': u' không ', '"k ': u' không ',' kh ':u' không ','kô':u' không ','hok':u' không ',' kp ': u' không phải ',u' kô ': u' không ', '"ko ': u' không ', u' ko ': u' không ', u' k ': u' không ', 'khong': u' không ', u' hok ': u' không ',
+        'cute': u' dễ thương ', ' vs ': u' với ', 'wa': ' quá ', 'wá': u' quá', 'j': u' gì ', '“': ' ',
+        ' sz ': u' cỡ ', 'size': u' cỡ ', u' đx ': u' được ', 'dk': u' được ', 'dc': u' được ', 'đk': u' được ',
+        'đc': u' được ','authentic': u' chuẩn chính hãng ',u' aut ': u' chuẩn chính hãng ', u' auth ': u' chuẩn chính hãng ', 'store': u' cửa hàng ',
+        'shop': u' cửa hàng ', 'sp': u' sản phẩm ', 'gud': u' tốt ','god': u' tốt ','wel done':' tốt ', 'good': u' tốt ', 'gút': u' tốt ',
+        'sấu': u' xấu ','gut': u' tốt ', u' tot ': u' tốt ', u' nice ': u' tốt ', 'perfect': 'rất tốt', 'bt': u' bình thường ',
+        'time': u' thời gian ', 'qá': u' quá ', u' ship ': u' giao hàng ', u' m ': u' mình ', u' mik ': u' mình ',
+        'ể': 'ể', 'product': 'sản phẩm', 'quality': 'chất lượng','chat':' chất ', 'excelent': 'hoàn hảo', 'bad': 'tệ','fresh': ' tươi ','sad': ' tệ ',
+        'date': u' hạn sử dụng ', 'hsd': u' hạn sử dụng ','quickly': u' nhanh ', 'quick': u' nhanh ','fast': u' nhanh ','delivery': u' giao hàng ',u' síp ': u' giao hàng ',
+        'beautiful': u' đẹp tuyệt vời ', u' tl ': u' trả lời ', u' r ': u' rồi ', u' shopE ': u' cửa hàng ',u' order ': u' đặt hàng ',
+        'chất lg': u' chất lượng ',u' sd ': u' sử dụng ',u' dt ': u' điện thoại ',u' nt ': u' nhắn tin ',u' tl ': u' trả lời ',u' sài ': u' xài ',u'bjo':u' bao giờ ',
+        'thik': u' thích ',u' sop ': u' cửa hàng ', ' fb ': ' facebook ', ' face ': ' facebook ', ' very ': u' rất ',u'quả ng ':u' quảng  ',
+        'dep': u' đẹp ',u' xau ': u' xấu ','delicious': u' ngon ', u'hàg': u' hàng ', u'qủa': u' quả ',
+        'iu': u' yêu ','fake': u' giả mạo ', 'trl': 'trả lời',
+        ' por ': u' tệ ',' poor ': u' tệ ', 'ib':u' nhắn tin ', 'rep':u' trả lời ',u'fback':' feedback ','fedback':' feedback ',
+    }
+    
+    for k, v in replace_list.items():
+        text = text.replace(k, v)
+
+    # chuyen punctuation thành space
+    translator = str.maketrans(string.punctuation, ' ' * len(string.punctuation))
+    text = text.translate(translator)
+
+    text = text.replace(u'"', u' ')
+    text = text.replace('🏻','')
+
+    text = word_tokenize(text, format="text")
+
+    return text
